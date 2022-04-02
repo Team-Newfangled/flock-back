d5012f4177ec2fd004c3329798a76d42680d4435
iqpizza6349 <iqpizza6349@gmail.com>
2022-04-02 14:45:49 +0900

    🌱:: 최초 커밋

---
+++ src/main/java/com/newfangled/flockbackend/FlockBackendApplication.java
@@ -0,0 +1,13 @@
+package com.newfangled.flockbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlockBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlockBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newfangled/flockbackend/FlockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newfangled.flockbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlockBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
