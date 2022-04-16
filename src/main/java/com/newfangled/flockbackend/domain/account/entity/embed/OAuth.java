58c25ab38f52527ced02b625fd8569197546401f
임재청 <iqpizza6349@gmail.com>
2022-04-16 11:47:59 +0900

    ⚗/🔱::#9 팀원 추가 및 유저 칼럼 변경 (#10)
    merge 진행합니다. -> issue close & Project Entity

---
+++ src/main/java/com/newfangled/flockbackend/domain/account/entity/embed/OAuth.java
@@ -0,0 +1,36 @@
+package com.newfangled.flockbackend.domain.account.entity.embed;
+
+import com.newfangled.flockbackend.domain.account.type.Provider;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@AllArgsConstructor @NoArgsConstructor
+@Builder
+@Embeddable
+public class OAuth {
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("'GOOGLE'")
+    private Provider provider;
+
+    @NotNull
+    @Column(name = "profile_img")
+    private String profileImage;
+}
