b4966526bde4556aaf970383833a66c3a908fc01
iqpizza6349 <iqpizza6349@gmail.com>
2022-06-16 09:49:53 +0900

    ⚠:: (#17) Validation Annotation 추가

--- src/main/java/com/newfangled/flockbackend/domain/project/dto/request/TodoRequestDto.java
+++ src/main/java/com/newfangled/flockbackend/domain/project/dto/request/TodoRequestDto.java
@@ -4,19 +4,26 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor @NoArgsConstructor
 public class TodoRequestDto {
 
+    @NotNull
     private String content;
 
     @JsonProperty("start-date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull
     private LocalDate startDate;
 
     @JsonProperty("end-date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull
     private LocalDate endDate;
 
 }
